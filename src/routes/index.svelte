9a2547e99395615c9839d03661ceb4fcb53033cd
ianheuser <ian.mccarthy@anheuser-busch.com>
2022-04-20 15:11:20 -0500

    Data Being dynamically added to initial brand page products

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -66,51 +66,53 @@
             console.dir(error);
         });
 
+        
     });
 
-	let slides = [
+	/*
+        let slides = [
 		{ title: 'BUDWEISER', text: 'Budweiser is a medium-bodied, flavorful, crisp American-style lager. Brewed with the best barley malt and a blend of premium hop varieties.', t1a:'ALC %', t1b:'CAL', t1c:'FAT', t1d:'CARBS',t2a:'5', t2b:'145', t2c:"0",t2d:'10.6', t3a:'BY VOL', t3b:'PER 12 OZ', t3c:'PER 12 OZ',t3d:'PER 8 OZ', image:'https://us.budweiser.com/img/home/desktop/02_Iconic_12ozBottle_T_529x654.png' },
 		{ title: 'BUDWEISER SUPREME', text: 'Budweiser is a medium-bodied, flavorful, crisp American-style lager. Brewed with the best barley malt and a blend of premium hop varieties.', t1a:'ALC %', t1b:'CAL', t1c:'FAT', t1d:'CARBS',t2a:'15', t2b:'155', t2c:"0",t2d:'10.6', t3a:'BY VOL', t3b:'PER 12 OZ', t3c:'PER 12 OZ',t3d:'PER 8 OZ', image:'https://us.budweiser.com/img/home/desktop/15_Supreme_12ozBottle_T_529x654.png' },
 		{ title: 'BUDWEISER ZERO', text: 'Budweiser is a medium-bodied, flavorful, crisp American-style lager. Brewed with the best barley malt and a blend of premium hop varieties.', t1a:'ALC %', t1b:'CAL', t1c:'FAT', t1d:'CARBS',t2a:'25', t2b:'45', t2c:"0",t2d:'10.6', t3a:'BY VOL', t3b:'PER 12 OZ', t3c:'PER 12 OZ',t3d:'PER 8 OZ', image: 'https://us.budweiser.com/img/home/desktop/Zero-12ozCan.png'},
-		
-	]
-	
-    //current slide variable
-	let cur = 0;
-	
-	function changeSlide(slide) {
-		cur = slide;
-	}
-	
-	const clamp = (number, min, max) => Math.min(Math.max(number, min), max);
-
-	/*
-	const transition_args = {
-		duration: 200,
-	}
-	*/
-	function prev(e) {
-		cur = clamp( --cur, 0, slides.length-1 )
-	}
-	
-	//
-	function next(e) {
-		cur = clamp( ++cur, 0, slides.length-1 )
-	}
-	
-	// set left arrow and right arrow to numbers that represent the key codes for the left and right buttons
-  	const ARROW_LEFT = 37;
-	const ARROW_RIGHT = 39;
-
-	// function to allow the left and right keys to control the slider
-	function handleShortcut(e) {
-        if (e.keyCode === ARROW_LEFT ) {
-			prev();
+            
+        ]
+        */
+        //current slide variable
+        let cur = 0;
+        
+        function changeSlide(slide) {
+            cur = slide;
         }
-		if (e.keyCode === ARROW_RIGHT ) {
-            next();
+        
+        const clamp = (number, min, max) => Math.min(Math.max(number, min), max);
+
+        /*
+        const transition_args = {
+            duration: 200,
         }
-    }
+        */
+        function prev(e) {
+            cur = clamp( --cur, 0, products.length-1 )
+        }
+        
+        //
+        function next(e) {
+            cur = clamp( ++cur, 0, products.length-1 )
+        }
+        
+        // set left arrow and right arrow to numbers that represent the key codes for the left and right buttons
+        const ARROW_LEFT = 37;
+        const ARROW_RIGHT = 39;
+
+        // function to allow the left and right keys to control the slider
+        function handleShortcut(e) {
+            if (e.keyCode === ARROW_LEFT ) {
+                prev();
+            }
+            if (e.keyCode === ARROW_RIGHT ) {
+                next();
+            }
+        }
 
 </script>
 
@@ -140,7 +142,7 @@
 <div class="extra-outer-wrapper">
 	<div class="outer-wrapper">
 		<div class="inner-wrapper">
-			{#each slides as slide, id}
+			{#each products as product, id}
 				{#if id === cur}
 				<div class="slide">
                     
@@ -148,34 +150,34 @@
                         &lt;
                     </button>
 					<div class="text-content">
-						<h1 class="title">{slide.title}</h1>
-						<p class="text">{slide.text}</p>
+						<h1 class="title">{product.fields.name}</h1>
+						<p class="text">{product.fields.extendedBlurb}</p>
 						<div class="spec-content">
 							<div class="box">
-								<h3 class="box-spec">{slide.t1a}</h3>
-								<h1>{slide.t2a}</h1>
-								<h3 class="box-text">{slide.t3a}</h3>
+								<h3 class="box-spec">ALC%</h3>
+								<h1>{product.fields.alcoholPercent}</h1>
+								<h3 class="box-text">BY VOL</h3>
 							</div>
 							<div class="box">
-								<h3 class="box-spec">{slide.t1b}</h3>
-								<h1>{slide.t2b}</h1>
-								<h3 class="box-text">{slide.t3b}</h3>
+								<h3 class="box-spec">CAL</h3>
+								<h1>{product.fields.caloriesPer12Oz}</h1>
+								<h3 class="box-text">PER 12 OZ</h3>
 							</div>
 							<div class="box">
-								<h3 class="box-spec">{slide.t1b}</h3>
-								<h1>{slide.t2c}</h1>
-								<h3 class="box-text">{slide.t3c}</h3>
+								<h3 class="box-spec">FAT</h3>
+								<h1>{product.fields.fat}</h1>
+								<h3 class="box-text">PER 12 OZ</h3>
 							</div>
 
 							<div class="box">
-								<h3 class="box-spec">{slide.t1d}</h3>
-								<h1>{slide.t2d}</h1>
-								<h3 class="box-text">{slide.t3d}</h3>
+								<h3 class="box-spec">CARBS</h3>
+								<h1>{product.fields.carbs}</h1>
+								<h3 class="box-text">PER 8 OZ</h3>
 							</div>
 						</div>
 					</div>
 					<div class="productImage">
-						<img src="{slide.image}" alt="">
+						<img src="{product.fields.profileImage.fields.file.url}" alt="">
 					</div>
                     <button class="nextAndBack" on:click="{()=>next()}">
                         &gt;
@@ -202,8 +204,8 @@
                 <button class="productLink" href="www.budweiser.com">Chelada</button>
                 <button class="productLink" href="www.budweiser.com">Supreme</button>
                     -->
-                {#each slides as slide, i}
-                    <button on:click={()=>changeSlide(i)} class="productLink" class:current={cur == i}>{slide.title}</button>
+                {#each products as product, i}
+                    <button on:click={()=>changeSlide(i)} class="productLink" class:current={cur == i}>{product.fields.name}</button>
                 {/each}
             </div>
 		</div>
